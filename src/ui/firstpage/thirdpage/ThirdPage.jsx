3f19c242339537adb40fa38d965bcd4dc99285d4
puthato <109648146+puthato@users.noreply.github.com>
2025-04-22 15:08:13 +0800

    Update ThirdPage.jsx

--- src/ui/firstpage/thirdpage/ThirdPage.jsx
+++ src/ui/firstpage/thirdpage/ThirdPage.jsx
@@ -59,7 +59,7 @@ function ThirdPage() {
   const [lastSpoken, setLastSpoken] = useState("");
 
   // ✅ Use your ngrok endpoint here
-  const API_URL = "https://a3f7-182-255-40-170.ngrok-free.app/detect";
+  const API_URL = "https://f900-182-255-40-170.ngrok-free.app/detect";
 
   const speakText = useCallback((text) => {
     if (!window.speechSynthesis || text === lastSpoken) return;
